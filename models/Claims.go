72b96b52bee7e287b07421ecd695675081140698
junanda.patihullah <junanda.patihullah@spesolution.com>
2024-01-31 09:18:44 +0700

    user and claims models

---
+++ models/Claims.go
@@ -0,0 +1,18 @@
+package models
+
+import "github.com/golang-jwt/jwt"
+
+type Claims struct {
+	Role string `json:"role"`
+	jwt.StandardClaims
+}
+
+type StandardClaim struct {
+	Audience  string `json:"aud,omitempty"`
+	ExpiresAt int64  `json:"exp,omitempty"`
+	Id        string `json:"jti,omitempty"`
+	IssuedAt  int64  `json:"iat,omitempty"`
+	Issuer    string `json:"iss,omitempty"`
+	NotBefore int64  `json:"nbf,omitempty"`
+	Subject   string `json:"sub,omitempty"`
+}
