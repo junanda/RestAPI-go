88fb0e625e5dfc249374e8fa91680be1716d90b7
anzoid <jpatihullah@gmail.com>
2024-02-11 20:27:01 +0700

    gracefull shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/junanda/golang-aa/config"
 	"github.com/junanda/golang-aa/controllers"
@@ -27,5 +35,37 @@ func main() {
 	authController.Handler(r)
 	berandaController.Handler(r)
 
-	r.Run(":8080")
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server.....")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+
+	dbc, _ := db.DB()
+	//catching ctx.Done()
+	select {
+	case <-ctx.Done():
+		dbc.Close()
+		log.Println("timeout of 5 seconds")
+	}
+	log.Println("Server exiting")
+
+	// r.Run(":8080")
 }
